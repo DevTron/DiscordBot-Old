5d3f900806ebeb7642ed58cca50d9863ab98fd61
TheDevTron <devtron6@me.com>
2018-04-04 23:56:18 -0400

    0.0.4
    Added Ban
    Added Kick
    Modified purge to display error in chat

--- commands/purge.js
+++ commands/purge.js
@@ -1,8 +1,10 @@
 exports.run = (client, message, args) => {
+    if (!message.guild.me.hasPermission("MANAGE_MESSAGES"))
+       return message.reply("I don't have permission to purge");
     const messagecount = parseInt(args.join(' '));
     message.channel.fetchMessages({
       limit: messagecount + 1
-    }).then(messages => message.channel.bulkDelete(messages));
+    }).then(messages => message.channel.bulkDelete(messages).catch(error => message.channel.send("Error: ${error}")));
   };
   
   exports.conf = {
