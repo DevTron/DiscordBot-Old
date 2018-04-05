05da5b479a6a267b764874918ecdb2558eba4c6e
TheDevTron <devtron6@me.com>
2018-04-05 00:15:38 -0400

    Testing GPG Signing

--- commands/stats.js
+++ commands/stats.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args, level) => { // eslint-disable-line no-unus
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
   message.channel.send(`= STATISTICS =
 • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Bot Uptime :: ${duration}
+• Uptime     :: ${duration}
 • Users      :: ${client.users.size.toLocaleString()}
 • Servers    :: ${client.guilds.size.toLocaleString()}
 • Channels   :: ${client.channels.size.toLocaleString()}
